38e947a14149d1272ba987143c290b205b8470b1
Vinayaka K V <me.vinayakakv@gmail.com>
2020-07-25 12:33:24 +0530

    Implement Station Label

---
+++ components/stationLabel.js
@@ -0,0 +1,14 @@
+import React from "react";
+import classNames from "classnames";
+
+export default function StationLabel(props) {
+    return (
+        <div className={classNames("StationLabel", {"selected": props.selected})} tabIndex={0} onClick={props.onClick}>
+            <span className="StationName">{props.name}</span>
+            <span className="StationPlace">{props.place}</span>
+            <svg width="26" height="40" viewBox="0 0 26 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M1 38L23 20.7087L1 2" stroke="black" strokeOpacity="0.79" strokeWidth="3"/>
+            </svg>
+        </div>
+    )
+}
