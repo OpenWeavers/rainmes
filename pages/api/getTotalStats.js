9c9a2f5659f90add0f3d80a49db4cc026d94666d
Vinayaka K V <me.vinayakakv@gmail.com>
2020-07-27 11:17:48 +0530

    Improve Card

--- pages/api/getTotalStats.js
+++ pages/api/getTotalStats.js
@@ -1,6 +1,5 @@
 const totalStats = [
     {value:200,unit:"in",description:"Maximum Annual Rainfall"},
-    {value:"Devaragudda", description: "Maximum Rainfall Recording Station"},
     {value:2018, description:"Year of Maximum Rainfall"},
     {value:12, description:"Stations"},
     {value:2, unit:"yrs", description:"Age of Oldest Data"}
