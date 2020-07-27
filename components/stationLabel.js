3f68257f3a8b3f1a56ed91d993d0eca8c85049ef
Vinayaka K V <me.vinayakakv@gmail.com>
2020-07-27 11:41:56 +0530

    Implement keypress on StationLabel

--- components/stationLabel.js
+++ components/stationLabel.js
@@ -3,7 +3,11 @@ import classNames from "classnames";
 
 export default function StationLabel(props) {
     return (
-        <div className={classNames("StationLabel", {"selected": props.selected})} tabIndex={0} onClick={props.onClick}>
+        <div className={classNames("StationLabel", {"selected": props.selected})}
+             tabIndex={0}
+             onClick={props.onClick}
+             onKeyDown={e => e.key === 'Enter' ? props.onClick() : null}
+        >
             <span className="StationName">{props.name}</span>
             <span className="StationPlace">{props.place}</span>
             <svg width="26" height="40" viewBox="0 0 26 40" fill="none" xmlns="http://www.w3.org/2000/svg">
