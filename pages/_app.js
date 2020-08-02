1d78b2eb31d440fe39a527c87224ef37ad143778
Vinayaka K V <me.vinayakakv@gmail.com>
2020-08-02 08:56:39 +0530

    Add redirectUri to env

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,13 @@
 import React from "react";
 import '../styles/App.scss'
 import { Auth0Provider } from '@auth0/auth0-react';
+import { useRouter } from 'next/router'
 
 export default ({ Component, pageProps }) => (
     <Auth0Provider
         domain={process.env.NEXT_PUBLIC_AUTH0_DOMAIN}
         clientId={process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID}
-        redirectUri="http://localhost:3000/userHome"
+        redirectUri={process.env.NEXT_PUBLIC_REDIRECT_URI}
     >
         <Component {...pageProps} />
     </Auth0Provider>
