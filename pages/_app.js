3e850cf2c7c25834c35e8ee9513f82a99ebd516e
Vinayaka K V <me.vinayakakv@gmail.com>
2020-08-02 08:58:39 +0530

    Revert "Add redirectUri to env"
    This reverts commit 1d78b2eb

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,12 @@
 import React from "react";
 import '../styles/App.scss'
 import { Auth0Provider } from '@auth0/auth0-react';
-import { useRouter } from 'next/router'
 
 export default ({ Component, pageProps }) => (
     <Auth0Provider
         domain={process.env.NEXT_PUBLIC_AUTH0_DOMAIN}
         clientId={process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID}
-        redirectUri={process.env.NEXT_PUBLIC_REDIRECT_URI}
+        redirectUri="http://localhost:3000/userHome"
     >
         <Component {...pageProps} />
     </Auth0Provider>
